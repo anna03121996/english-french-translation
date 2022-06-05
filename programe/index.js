5d82832781d4f5a16a72890b693ace43b945966c
anna03121996 <101927808+anna03121996@users.noreply.github.com>
2022-06-05 06:46:23 +0530

    commit

--- programe/index.js
+++ programe/index.js
@@ -1,51 +1,47 @@
-let upload = document.getElementById("upload");
-let inputFileText;
-let dictionary = document.getElementById("dictionary");
-let dictionaryFileText;
+let startTime = new Date().getTime();
+translate();
+let finishTime = new Date().getTime();
+document.getElementById("time").innerHTML=`Time taken to process  ${(finishTime - startTime) / 1000} seconds`;
+document.getElementById("memory").innerHTML=` Memory taken to process ${(performance.memory.usedJSHeapSize) / 1024 / 1024}`;
 
-function myFunction() {
+function translate() {
 
-    let startTime = performance.now();
-    // get the input file text
+    // read all text files
+    Promise.all([
+        fetch('t8.shakespeare.txt').then(x => x.text()),
+        fetch('french_dictionary.csv').then(x => x.text()),
+        fetch('find_words.txt').then(x => x.text())
+    ]).then(([input, dictionary, find]) => {
 
-    let fr = new FileReader();
-    fr.readAsText(upload.files[0]);
-    fr.onload = function () {
-        inputFileText = fr.result.toLowerCase(); //console.log(inputFileText);
+        // modify the text for our requirement
+        let inputFileText = input.toLowerCase();
+        let dictionaryFileText = dictionary.replace(/[\r\n]+/gm, ",").split(",");;
+        let findWordsFileText = find.replace(/[\n]+/gm, ",").split(",");;
 
-        // get the english and french dictionary
-        let fr2 = new FileReader();
-        fr2.readAsText(dictionary.files[0]);
-        fr2.onload = function () {
-            dictionaryFileText = fr2.result.replace(/[\r]+/gm, ",").replace(/[\n]+/gm, ",").split(",");
-
-            let frenchArr = [], englishArr = [];
-            for (let i = 0; i < dictionaryFileText.length; i++) {
-                if ((i + 2) % 2 == 0) { englishArr.push(dictionaryFileText[i]); } 
-                else { frenchArr.push(dictionaryFileText[i]); }
-            }//console.log(frenchArr);
-
-            let dictionaryObj = {};
-            for (var i = 0; i < englishArr.length; i++) {
-                dictionaryObj[englishArr[i]] = frenchArr[i];
-            }//console.log(dictionaryObj); 
-
-            let output = inputFileText.replace(/(\w+)/g, (value,key)=>dictionaryObj[key]||value);
-            document.getElementById("result").innerHTML = output;
-            
-            // gettting frequency of translated words
-
-            let text = "<tr><th>English</th><th>French</th><th>Count</th></tr>";
-            englishArr.forEach(function (englishWord,indexOfEnglishWord) {
-                let frenchWord = frenchArr[indexOfEnglishWord];
-                let count = inputFileText.match(new RegExp(englishWord,"g")).length;
-                text += `<tr> <td>${englishWord}</td> <td>${frenchWord}</td> <td>${count}<td> </tr>`;
-                document.getElementById("frequency").innerHTML = text;
-            });  //let result = (inputFileText!= output)?true:false; console.log(result); // for testing purpose (the translation function is success or failed )
+        // creating dictionary object
+        let frenchArr = [], englishArr = [];
+        for (let i = 0; i < dictionaryFileText.length; i++) {
+            if ((i + 2) % 2 == 0) { englishArr.push(dictionaryFileText[i]); }
+            else { frenchArr.push(dictionaryFileText[i]); }
         }
-    }
 
-    let finishTime = performance.now();
-    document.getElementById("time").innerHTML = (`Time taken to process  ${(finishTime - startTime)/1000} seconds`);
-    document.getElementById("memory").innerHTML = (`Memory taken to process ${performance.memory.usedJSHeapSize / 1024/1024} mb`);
+        let dictionaryObj = {};
+        for (var i = 0; i < englishArr.length; i++) {
+            dictionaryObj[englishArr[i]] = frenchArr[i];
+        }//console.log(dictionaryObj); 
+
+        // replace the english to french using the dictionary
+        let output = inputFileText.replace(/(\w+)/g, (value, key) => dictionaryObj[key] || value);
+        document.getElementById("result").innerHTML = output;
+
+        // getting frequency of each words
+        let text = "<tr><th> &#160; English</th><th>French</th><th>Count</th></tr>";
+        findWordsFileText.forEach(function (englishWord, indexOfEnglishWord) {
+            let frenchWord = frenchArr[indexOfEnglishWord];
+            let count = inputFileText.match(new RegExp(englishWord, "g")).length;
+            text += `<tr> <td> &#160; ${englishWord}</td> <td>${frenchWord}</td> <td>${count}<td> </tr>`;
+            document.getElementById("frequency").innerHTML = text;
+        });
+    });
+
 }
